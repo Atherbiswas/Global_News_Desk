21c48e59236deba32ef34453af490a74e39bd635
Ather1230 <ather.biswas0057@gmail.com>
2022-09-03 02:59:40 +0600

    news category added

--- script/index.js
+++ script/index.js
@@ -7,13 +7,41 @@ const loadAllCategory = () =>{
 // Display to Ui all category list...
 const displayAllCategory = categories => {
     for(const category of categories){
+        // console.log(category)
         const allCategory = document.getElementById('all-category');
         const li = document.createElement('li');
         li.classList.add('nav-item')
         li.innerHTML = `
-        <a class="nav-link active p-2 mx-3 fs-5 text-black-50 fw-semibold" aria-current="page" href="#">${category.category_name}</a>
+        <a class="nav-link active p-2 mx-3 fs-5 text-black-50 fw-semibold" aria-current="page" onclick="loadPerCategoryNews('${category.category_id}')" href="#">${category.category_name}</a>
         `;
         allCategory.appendChild(li);
     }
 }
-  
+// Every category load news from api...
+const loadPerCategoryNews = (category_id) => {
+    const url = `https://openapi.programming-hero.com/api/news/category/${category_id}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayPercategoryNews(data.data[0]))
+}
+const displayPercategoryNews = category => {
+    console.log(category)
+    const newsContainer = document.getElementById('news-container');
+    newsContainer.innerHTML = `
+    <div class="col-md-4">
+        <img src="${category.thumbnail_url}" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+        <div class="card-body">
+            <h5 class="card-title">${category.title}</h5>
+            <p class="card-text">${category.details}</p>
+            <div class="player">
+            <img src="..." class="img-fluid" alt="...">
+            <p class="player-name">John Smith</p>
+            </div>
+        </div>
+    </div>
+    `;
+}
+
+ 
